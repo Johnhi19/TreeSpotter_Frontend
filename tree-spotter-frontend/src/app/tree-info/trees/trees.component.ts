a11f6e5a1452d5d171fe7927a888967fff99aa72
Johnhi19 <johannes.hierlinger@gmail.com>
2025-11-27 21:46:46 +0100

    Trees-Component: Update design and what is displayed when clicking on a tree

--- tree-spotter-frontend/src/app/tree-info/trees/trees.component.ts
+++ tree-spotter-frontend/src/app/tree-info/trees/trees.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { Tree } from '../../models/tree';
@@ -7,16 +7,38 @@ import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-trees',
-  imports: [RouterLink, RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './trees.component.html',
   styleUrl: './trees.component.scss'
 })
 export class TreesComponent {
   tree: Tree | null = null;
+  treeId: number | null = null;
+  meadowId: number | null = null;
 
-  constructor(private apiService: ApiService) {}
-  
-  fetchData(): void {
-    this.apiService.getTreeById(1).subscribe((data) => { this.tree = data; });
+
+  constructor(private route: ActivatedRoute, private apiService: ApiService, private router: Router) {}
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    this.treeId = idParam ? Number(idParam) : null;
+
+    // Get meadowId from query params
+    this.route.queryParams.subscribe(params => {
+      this.meadowId = params['meadowId'] ? Number(params['meadowId']) : null;
+    });
+
+    if (this.treeId){
+      console.log(`Fetching data for tree ID: ${this.treeId}`);
+      this.apiService.getTreeById(this.treeId).subscribe((data) => { this.tree = data; });
+    }
+  }
+
+  goBackToMeadow() {
+    if (this.meadowId) {
+      this.router.navigate(['/meadow', this.meadowId]);
+    } else {
+      this.router.navigate(['/']);
+    }
   }
 }
