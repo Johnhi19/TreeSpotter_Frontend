73c17b61bd038fd544333f56bb002e8895416a5a
Johnhi19 <johannes.hierlinger@gmail.com>
2025-12-01 21:49:07 +0100

    Add-Meadow-Component: Add functionality to add a new meadow

--- tree-spotter-frontend/src/app/meadow-info/add-meadow/add-meadow.component.ts
+++ tree-spotter-frontend/src/app/meadow-info/add-meadow/add-meadow.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../api.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Meadow } from '../../models/meadow';
 
 @Component({
   selector: 'app-add-meadow',
-  imports: [],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule],
   templateUrl: './add-meadow.component.html',
   styleUrl: './add-meadow.component.scss'
 })
 export class AddMeadowComponent {
+  meadow = {
+    name: '',
+    location: '',
+    size: [0, 0],
+  };
 
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+  
+  onSubmit() {
+    const newMeadow: Meadow = {
+      id: 0,
+      name: this.meadow.name,
+      treeIds: [],
+      size: [Number(this.meadow.size[0]), Number(this.meadow.size[1])],
+      location: this.meadow.location,
+    };
+    
+    this.apiService.insertMeadow(newMeadow).subscribe({
+      next: (response: any) => {
+        console.log('Meadow added successfully', response);
+        this.router.navigate(['/meadow', response.id]);
+      },
+      error: (error: any) => {
+        console.error('Error adding meadow', error);
+      }
+    });
+  }
+  
+  onCancel() {
+    this.router.navigate(['/']);
+  }
 }
