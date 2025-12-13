f6b2ed25e63f8acbcd68cedb21d5986304fca91c
Johnhi19 <johannes.hierlinger@gmail.com>
2025-12-13 18:11:55 +0100

    Add logout button to the header

--- tree-spotter-frontend/src/app/app.component.ts
+++ tree-spotter-frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
+import { AuthService } from './authentication/authService';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,14 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'tree-spotter-frontend';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
   
   goHome() {
     this.router.navigate(['/']);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
