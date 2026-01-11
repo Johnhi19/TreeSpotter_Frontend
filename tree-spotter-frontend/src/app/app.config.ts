d8d1982d97dc7ae7382d04995caf3032865f6cc5
Johnhi19 <johannes.hierlinger@gmail.com>
2026-01-11 22:08:00 +0100

    App Config: Add Translate service with the language german (default is english)

--- tree-spotter-frontend/src/app/app.config.ts
+++ tree-spotter-frontend/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import {provideTranslateService, TranslateService} from "@ngx-translate/core";
+import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
 import { authInterceptor } from './authentication/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
@@ -11,5 +13,14 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(withEventReplay()), 
-    provideHttpClient(withInterceptors([authInterceptor])) ]
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideTranslateService({
+        lang: 'de',
+        fallbackLang: 'en',
+        loader: provideTranslateHttpLoader({
+          prefix: '/i18n/',
+          suffix: '.json'
+        })
+      }), 
+    ]
 };
