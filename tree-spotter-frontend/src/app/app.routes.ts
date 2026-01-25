63f2f53c6a04c45c2824ea32ca80e285a17028b5
Johnhi19 <johannes.hierlinger@gmail.com>
2026-01-25 13:27:58 +0100

    App-Routes: Add route to upload an image for the trees

--- tree-spotter-frontend/src/app/app.routes.ts
+++ tree-spotter-frontend/src/app/app.routes.ts
@@ -8,10 +8,12 @@ import { AddMeadowComponent } from './meadow-info/add-meadow/add-meadow.componen
 import { AuthComponent } from './authentication/components/auth.component';
 import { RegisterComponent } from './authentication/components/register/register.component';
 import { AuthGuard } from './authentication/auth.guard';
+import { UploadTreeImageComponent } from './upload-tree-image/upload-tree-image.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent, canActivate: [AuthGuard]},
   {path: 'trees/:id', component: TreesComponent, canActivate: [AuthGuard]},
+  {path: 'trees/:id/uploadImage', component: UploadTreeImageComponent, canActivate: [AuthGuard]},
   {path: 'meadow/:id', component: MeadowDetailComponent, canActivate: [AuthGuard]},
   {path: 'add-tree/:meadowId', component: AddTreeComponent, canActivate: [AuthGuard]},
   {path: 'add-meadow', component: AddMeadowComponent, canActivate: [AuthGuard]},
